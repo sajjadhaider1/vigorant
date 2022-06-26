f64a9f3659ca896ba0ef2b462f50067eaa1e53f0
Sajjad Haider <sajjad.haider.1@gmail.com>
2022-06-26 22:04:41 +0500

    update

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+// markup
+const Header = () => {
+  return (
+      <nav className="flex items-center justify-between py-[14px] px-[150px] w-full shadow-md sticky top-0">
+
+        <div className="flex items-end gap-2">
+          <StaticImage alt="Vigorant Logo" src="../images/logo.svg" layout="fixed"/>
+          <span className="text-[21.25px] font-[700] tracking-wide leading-[26px] font-montserrat">VIGORANT</span>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <span className="text-[#697492] text-xl font-[450] tracking-wide ">
+            {" "}
+            Got a question? Just call us at +1 833 641 2200{" "}
+          </span>
+          <button className="bg-[#4975E9] text-white text-base px-[26px] py-[13px] rounded-md ">
+            {" "}
+            Get free consultation{" "}
+          </button>
+        </div>
+
+      </nav>
+  );
+};
+
+export default Header;
