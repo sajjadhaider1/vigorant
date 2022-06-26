1ebf58779441b2eb559f8c6a7580482beba3725d
Sajjad Haider <sajjad.haider.1@gmail.com>
2022-06-26 06:28:18 +0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `vigorant`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", 'gatsby-plugin-postcss','gatsby-plugin-postcss', "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
