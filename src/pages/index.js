a61cdba31900b45b1e7acda5c59cb27bd71db1a7
Sajjad Haider <sajjad.haider.1@gmail.com>
2022-06-26 07:21:28 +0500

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,15 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-import logo from "../images/logotype.svg"
 
-// content
-const content = {
-  navbar:  {
-    text: "Tutorial",
-    logo: "../images/logotype.svg",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  section1:
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  }
-}
 
 // markup
 const IndexPage = () => {
   return (
     <main className="font-circular">
 
-      <nav className="flex items-center justify-between px-20 my-6">
-        <StaticImage className="" alt="Vigorant Logo" src="../images/logotype.svg" layout="fixed" height={40} />
-        <div className="flex items-center space-x-2"> 
+      <nav className="flex items-center justify-between my-[30px] px-[150px]">
+        <StaticImage className="" alt="Vigorant Logo" src="../images/logotype.svg" layout="fixed"  />
+        <div className="flex items-center gap-5"> 
           <span className="text-[#697492] text-xl "> Got a question? Just call us at +1 833 641 2200 </span>
           <button className="bg-[#4975E9] text-white text-base px-[26px] py-[13px] rounded-md "> Get free consultation </button>
         </div>
