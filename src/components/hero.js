8ef865af7de23c6736279aa6e5d64f23ba0be122
Sajjad Haider <sajjad.haider.1@gmail.com>
2022-06-27 07:05:13 +0500

    update

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,13 +5,13 @@ const Hero = () => {
   return (
     <section className="my-6">
       <div className="rounded-[100px] bg-[#E9F0FF] py-28 flex justify-between items-center">
-        <div className="bg-[#FF9F43] rounded-full h-[30px] w-[30px] fixed left-12 "></div>
-        <div className="w-full mx-20 space-y-5 lg:w-1/2">
+        <div className="bg-[#FF9F43] rounded-full h-[30px] w-[30px] absolute left-12 "></div>
+        <div className="w-full pr-12 mx-20 space-y-5 lg:w-1/2">
           <div className="text-6xl font-medium">
             Make your website load faster, optimize your images and boost your
             SEO.
           </div>
-          <div className="text-xl font-medium text-[#697492]">
+          <div className="text-xl font-medium text-[#697492] lg:pr-20">
             Increase the number of visitors to your website by appearing
             organically at the top of the major search engines results pages.
           </div>
@@ -25,7 +25,7 @@ const Hero = () => {
           </div>
         </div>
         <StaticImage
-          className="fixed right-0 hidden lg:flex"
+          className="absolute right-0 hidden lg:flex"
           src="../images/heroright.png"
           alt="Increase the number of visitors to your website."
         />
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import * as React from "react";
 import Header from "../components/header";
 import Hero from "../components/hero";
 
-// markup
 const IndexPage = () => {
   return (
     <main className="mx-[66px] max-w-[1440px]">
@@ -14,7 +13,26 @@ const IndexPage = () => {
 
       <Hero />
 
-
+      <section className="flex flex-col items-center my-20 space-y-6 text-center">
+        <div className="w-3/4 text-5xl font-semibold">
+          Increase your appearing <br/> at the top of the major search engines
+          </div>
+        <div className="text-[#697492] font-medium text-xl w-3/4">The stats are in our favour. Vigorant doesn't just help you with your marketing, we get you customer relationships that last.</div>
+        <div className="flex bg-[#EBF6ED] justify-between w-4/5 rounded-3xl border border-[#6F8273] py-10 px-20">
+          <div className="space-y-2">
+            <div className="text-[#163F1D] font-bold text-5xl ">+153%</div>
+            <div className="text-[#6F8273] font-semibold text-base">Increased <br/> Brand Awareness</div>
+          </div>
+          <div className="space-y-2">
+            <div className="text-[#163F1D] font-bold text-5xl ">+98%</div>
+            <div className="text-[#6F8273] font-semibold text-base">Increased <br/> Web Visitors</div>
+          </div>
+          <div className="space-y-2">
+            <div className="text-[#163F1D] font-bold text-5xl ">+175%</div>
+            <div className="text-[#6F8273] font-semibold text-base">Increased <br/> Conversion Rates</div>
+          </div>
+        </div>
+      </section>
 
     </main>
   );
